afdfad425a2caac5e95606f6fd83fddfe50b4cc1
Anthony Mace <amace93@gmail.com>
2013-09-28 19:26:05 -0700

    inital commit

---
+++ futval.py
@@ -0,0 +1,6 @@
+__author__ = 'Mace'
+import graphics
+from graphics import *
+princ = eval(input("Enter initial principal: "))
+rate = eval(input("Enter the interest rate (%): ")) / 100
+timeSpane = eval(input("Enter the amount of years investing: "))
