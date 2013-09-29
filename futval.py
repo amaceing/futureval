8473e8704705274f06178f03f6a6cbc2386a2a64
Anthony Mace <amace93@gmail.com>
2013-09-28 19:40:47 -0700

    fixed typos

--- futval.py
+++ futval.py
@@ -2,7 +2,7 @@ __author__ = 'Mace'
 import graphics
 from graphics import *
 princ = eval(input("Enter initial principal: "))
-rate = eval(input("Enter the interest rate (%): ")) / 100)
-for i in range(t10):
+rate = eval(input("Enter the interest rate (%): ")) / 100
+for i in range(10):
     princ = princ * (1 + rate)
     print(princ)
